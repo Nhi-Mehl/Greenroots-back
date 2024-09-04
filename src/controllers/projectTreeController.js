c007988c66896b5496abe4b86d22ec2c37201ac3
Julien-BRUNO <julien.bruno31@gmail.com>
2024-09-04 15:18:56 +0200

    feat: Route project_trees all
    route get all project_trees is functional

---
+++ src/controllers/projectTreeController.js
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+import { Project_tree } from '../models/index.js';
+import { NotFoundError } from '../utils/errors.js';
+
+const projectTreeSchema = z.object({
+    name: z.string().min(1),
+    description: z.string().min(1),
+    picture: z.string().min(1),
+    status: z.string().min(1),
+    city: z.string().min(1),
+    country: z.string().min(1),
+    continent: z.string().min(1),
+});
+
+const projectTreeController = {
+
+    async getAll(req, res) {
+        const projectTrees = await Project_tree.findAll({
+            order: [['id', 'ASC']],
+        });
+        res.json(projectTrees);
+        console.log(projectTrees);
+    },
+
+};
+
+export default projectTreeController;
--- src/router/projectTree.js
+++ src/router/projectTree.js
@@ -1,6 +1,11 @@
 import { Router } from 'express';
+import projectTreeController from '../controllers/projectTreeController.js';
+import cw from '../utils/controllerWrapper.js'
 
 const projectTreeRouter = Router();
+
+// route qui permet de récupérer tous les projets du site
+projectTreeRouter.get('/', cw(projectTreeController.getAll));
 
 
 export default projectTreeRouter;
