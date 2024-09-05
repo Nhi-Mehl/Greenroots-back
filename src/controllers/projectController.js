f704dd4b5c8885a03de949840f50f4f6f85c4a7b
igor-babych <igor.babych@oclock.school>
2024-09-05 14:42:21 +0200

    feat: New Project Routes
    Adding new routes and modification of old routes

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 import { Project } from '../models/index.js';
 import { NotFoundError } from '../utils/errors.js';
+import { Sequelize } from 'sequelize';
+//import { Association } from 'sequelize';
 
 const projectSchema = z.object({
     name: z.string().min(1),
@@ -12,14 +14,62 @@ const projectSchema = z.object({
     continent: z.string().min(1),
   });
 
+
+  
 const projectController = {
+
+    // controller pour récupérer tous les projets de la BDD
 
     async getAll(req, res) {
         const projects = await Project.findAll({
           order: [['name', 'ASC']],
         });
         res.json(projects);
-        console.log(projects);
+      },
+
+      // controller pour récupérer un seul projet en fonction de son id
+
+      async getOne(req, res) {
+        const { id } = req.params;
+
+        const oneProject = await Project.findByPk(id);
+        res.json(oneProject);
+      },
+
+      // controller pour récupérer 3 projets de façon aléatoire
+
+      
+      async getThreeRandomProjects(req, res) {
+        const threeRandomProjects = await Project.findAll({
+          //order: [['name', 'ASC']],
+          order: Sequelize.literal('RANDOM()'),
+          limit: 3
+        });
+        res.json(threeRandomProjects);
+      },
+      
+
+      // controller pour récupérer un seul projet en fonction de son id en incluant les arbres et les espèces du projet
+
+      
+      async getOneProjectTrees(req, res) {
+        const { id } = req.params;
+
+        const oneProject = await Project.findByPk(id, {
+            include: [
+                {
+                    association: 'project_trees',
+                    include: [
+                        {
+                            association: 'species'
+                        }
+                    ]
+                }
+            ],
+          // order: [['name', 'ASC']],
+        });
+        res.json(oneProject);
+        console.log(oneProject);
       },
 
 };
--- src/router/project.js
+++ src/router/project.js
@@ -6,7 +6,12 @@ const projectRouter = Router();
 
 // route qui permet de récupérer tous les projets du site
 projectRouter.get('/', cw(projectController.getAll));
-
+// permet de récupérer 3 projets random de la page accueil
+projectRouter.get('/highlights', cw(projectController.getThreeRandomProjects));
+// permet qui permet de récupérer un seul projet avec les arbres associés 
+projectRouter.get('/:id', cw(projectController.getOne));
+//permet de récupérer les arbres d'un projet
+projectRouter.get('/:id/project_trees', cw(projectController.getOneProjectTrees));
 
 
 export default projectRouter;
