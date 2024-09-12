286082af5b6056062e837c0d66ce410cc1c43a70
igor-babych <igor.babych@oclock.school>
2024-09-12 16:23:43 +0200

    feat: User's orders
    the user's orders route is created

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -12,6 +12,23 @@ const orderController = {
 
     async getOneLines(req, res) {
 
+    },
+
+    // Permet de récupérer tous les orders d'un utilisateur
+
+    async getAllUserOrders(req, res) {
+        console.log(req.user)
+
+        const userOrders = await Order.findAll({
+            where: { user_id: req.user.id },
+            order: [['created_at', 'ASC']]
+        });
+        if(userOrders.user_id !== req.user.id) {
+            return res.status(403).json({ message: 'You are not allowed to see this order'});
+        };
+        if (!userOrders || userOrders.length === 0) {
+            return res.status(404).json({ message: 'No orders found for this user' });
+        }
     }
 };
 
--- src/router/orderRouter.js
+++ src/router/orderRouter.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import cw from '../utils/controllerWrapper.js';
 import orderController from '../controllers/orderController.js';
+import isLoggedIn from '../middlewares/isLoggedIn.js';
 
 const orderRouter = Router();
 
 // route qui permet de récupérer toutes les commandes passées
 orderRouter.get('/', cw(orderController.getAll));
+
+// route pour récupérer toutes les commandes d'un utilisateur
+orderRouter.get('/:user_id', isLoggedIn, cw(orderController.getAllUserOrders));
 
 export default orderRouter;
