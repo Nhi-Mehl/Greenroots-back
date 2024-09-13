820a6985f379b019ac700346efbfd8270a4b922a
igor-babych <igor.babych@oclock.school>
2024-09-13 15:14:19 +0200

    feat: Route post create order
    This route is created and functional

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,4 +1,6 @@
-import { Order } from '../models/index.js';
+import { Sequelize } from 'sequelize';
+import { Order, Order_line } from '../models/index.js';
+
 
 const orderController = {
     // controller pour récupérer toutes les commandes de la BDD
@@ -25,6 +27,32 @@ const orderController = {
         if (!userOrders || userOrders.length === 0) {
             return res.status(404).json({ message: 'No orders found for this user' });
         }
+    },
+
+    // Permet de créer un order avec ses order_lines
+
+    async createOrder(req, res) {
+        
+        const { amount, orderLine } = req.body;
+
+
+        const newOrder = await Order.create({
+            user_id: req.user.id,
+            amount: amount
+          });
+
+        const newOrderLines = await Promise.all(orderLine.map(async line => {
+            return await Order_line.create({
+              order_id: newOrder.id, 
+              project_tree_id: line.project_tree_id,
+              quantity: line.quantity,
+              amount: line.amount
+            });
+        }));
+        
+        return res.status(201).json({ message: 'Order created successfully', newOrder, newOrderLines });
+    
+
     }
 };
 
