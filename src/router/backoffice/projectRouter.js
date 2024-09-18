834834c8886b37895f8aee1d6240f715cb61373b
Julien-BRUNO <julien.bruno31@gmail.com>
2024-09-18 14:33:14 +0200

    feat: Adding form and features to create a new project

--- src/router/backoffice/projectRouter.js
+++ src/router/backoffice/projectRouter.js
@@ -6,6 +6,8 @@ const projectRouter = Router();
 
 // route qui permet de récupérer tous les projets du site
 projectRouter.get('/', cw(projectController.getAll));
+projectRouter.get('/new_project', cw(projectController.newForm));
+projectRouter.post('/new_project', cw(projectController.create));
 projectRouter.get('/:id', cw(projectController.getOne));
 projectRouter.post('/:id', cw(projectController.updateOne));
 
